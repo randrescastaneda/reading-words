cb8d46433c3954c72d752faf3a82d42a979a3069
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-04-21 08:22:34 -0400

    add palabras

---
+++ R/sp_palabras.R
@@ -0,0 +1,26 @@
+url <- "https://corpus.rae.es/frec/5000_formas.TXT"
+
+df <- read_tsv(url,
+               col_names = c("orden", "to_read", "freqa", "freqr"),
+               skip = 1,
+               col_select = "to_read")
+
+group    <- vector(mode = "character", length = nrow(df))
+n_groups <- c(100, 400, 1000, 1500, 2000)
+j <- 1
+for (i in seq_along(n_groups)) {
+  if (i == 1)  {
+    lim <- n_groups[i]
+  } else {
+    lim <- n_groups[i] +  lim
+  }
+  group[j:lim] <- glue("Nivel {i}")
+  j <- lim + 1
+}
+
+df$group <- group
+setDT(df)
+
+fwrite(df, "static/spanish/palabras.csv")
+
+
